0b9762e7f822c278f6850b93c920e7f939b95dcc
Arthur Dent <netzego@gmail.com>
2016-01-23 00:20:00 +0100

    filewalk, isaudio, read_tags, merge_tags, print_tags

--- tagcat.py
+++ tagcat.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
 
 
 import os
+import taglib
 
 
 def filewalk(ls, recursiv=False, test=os.path.isfile):
     """Finds valid files from a list of path.  Valid files are defined by the
     test function.  If an item of the given list is a directorie and recursiv
-    is set True, filewalk finds valid files recursivly in that directorie.
+    is set True, filewalk finds valid files recursively in that directorie.
+
+    helllo
 
     Args:
         ls: A list of files/directories.  ``list[str]``
@@ -16,7 +20,7 @@ def filewalk(ls, recursiv=False, test=os.path.isfile):
         test: A function to test against each file/string.  ``func``
 
     Returns:
-        list:       A list of files.
+        list: A list of absolute filenames that passes ``test``.
 
     Raises:
         TypeError:  If an argument has not an excepted instance.
@@ -33,7 +37,6 @@ def filewalk(ls, recursiv=False, test=os.path.isfile):
         raise TypeError("``test`` is not a ``function``")
 
     def recursion(index, retval):
-        # pylint: disable=missing-docstring
         # pylint: disable=unused-variable
 
         # basecase
@@ -54,3 +57,95 @@ def filewalk(ls, recursiv=False, test=os.path.isfile):
 
     # start and set default values for `index` and `retval`
     return recursion(0, [])
+
+
+def isaudio(fn):
+    """Test if `fn` is a valid audiofile.
+
+    Args:
+        fn: A filename.
+
+    Returns:
+        bool: True if `fn` is a audiofile, False otherwise.
+
+    Raises:
+        ValueType: If `fn` is not an instance from `str`.
+
+    """
+
+    if not isinstance(fn, str):
+        raise TypeError("``fn`` is not a ``str``")
+
+    if not os.path.isfile(fn):
+        return False
+
+    elif os.path.isfile(fn) and os.path.islink(fn):
+        return False
+
+    elif not fn.endswith((".mp3", ".MP3", ".flac", ".FLAC")):
+        return False
+
+    return True
+
+
+def read_tags(fn):
+    """Reads tags from an audio file.
+
+    Returns:
+        dict: A dictinary with the tag, value pairs.
+
+    Raises:
+        TypeError: If ``fn`` is not a ``str``
+
+    """
+
+    try:
+        afile = taglib.File(fn)
+        tags = afile.tags
+        afile.close()
+    except:
+        tags = {}
+
+    return tags
+
+
+def merge_tags(ls):
+    """Merges all tags together in one dict.
+    """
+
+    def recursion(index, retval, memo):
+
+        # basecase
+        if index == len(ls):
+            return retval
+
+        # do stuff to retval and memo
+        tags = read_tags(ls[index])
+        for tag in set(list(retval.keys()) + list(tags.keys())) - set(memo):
+            try:
+                if retval[tag] != tags[tag]:
+                    raise KeyError
+            except KeyError:
+                retval[tag] = ["~"]
+                memo.append(tag)
+
+        # recursion
+        return recursion(index+1, retval, memo)
+
+    # start recursion and set default values
+    return recursion(1, read_tags(ls[0]), [])
+
+
+def print_tags(tags):
+    """Prints tags to stdout
+    """
+    s = ""
+    for tag, value in tags.items():
+        s += "{0}: {1}\n".format(tag, ", ".join(value))
+    print(s)
+
+
+if __name__ == "__main__":
+    filelist = filewalk(os.sys.argv[1:], recursiv=True, test=isaudio)
+    mtags = merge_tags(filelist)
+    print_tags(mtags)
